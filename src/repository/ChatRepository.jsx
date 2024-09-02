7bd4f9de5cc589c878ddc148dc86e85012579e01
ppaunovski <paunovskipavel@gmail.com>
2024-09-02 21:03:59 +0200

    quick fix

--- src/repository/ChatRepository.jsx
+++ src/repository/ChatRepository.jsx
@@ -1,30 +1,34 @@
 import axios from "../axios/axios";
 
-const token = localStorage.getItem('authToken');
-const userDTO = JSON.parse(localStorage.getItem('userDTO'));
-const userId = userDTO.id
-console.log(userId)
+const token = localStorage.getItem("authToken");
+const userDTO = JSON.parse(localStorage.getItem("userDTO"));
+const userId = userDTO != undefined ? userDTO.id : 0;
+console.log(userId);
 const ChatRepository = {
-    getAllChats: () => {
-        return axios.get(`/chat-room/all`);
-    },
-    getAllChatsForUser: () => {
-        return axios.get(`/chat-room/all-for-user/${userId}`, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        })
-    },
-    createChatRoom: (chatRoomData) => {
-        return axios.post("/chat-room/create-chat-room", {
-            userId: userId,
-            title: chatRoomData.title
-        }, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        });
-    }
-}
+  getAllChats: () => {
+    return axios.get(`/chat-room/all`);
+  },
+  getAllChatsForUser: () => {
+    return axios.get(`/chat-room/all-for-user/${userId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  },
+  createChatRoom: (chatRoomData) => {
+    return axios.post(
+      "/chat-room/create-chat-room",
+      {
+        userId: userId,
+        title: chatRoomData.title,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+  },
+};
 
 export default ChatRepository;
